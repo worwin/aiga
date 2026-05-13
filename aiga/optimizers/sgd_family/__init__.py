34ae60b0cd127c6c63f6f62f3d57025f2d7a31c1
Joshua W. Windle <joshua.w.windle@gmail.com>
2026-05-13 11:32:42 +0200

    Joshua Windle - organize optimizer families

---
+++ aiga/optimizers/sgd_family/__init__.py
@@ -0,0 +1,11 @@
+"""SGD optimizer family.
+
+This package contains optimizers based on stochastic gradient descent.
+
+Planned optimizers:
+    SGD: Plain stochastic gradient descent.
+    MomentumSGD: SGD with momentum.
+    NesterovSGD: SGD with Nesterov accelerated gradient.
+"""
+
+from .sgd import SGD
