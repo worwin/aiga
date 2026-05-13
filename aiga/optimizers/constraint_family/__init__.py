a79560a9ccb8ffcc71fc2253bf5ec150afdefa23
Joshua W. Windle <joshua.w.windle@gmail.com>
2026-05-13 11:34:17 +0200

    Joshua Windle - add constrained optimizer package

---
+++ aiga/optimizers/constraint_family/__init__.py
@@ -0,0 +1,9 @@
+"""Constrained optimizer family.
+
+This package is reserved for optimization methods that enforce constraints
+or use proximal/projection steps during parameter updates.
+
+Planned optimizers:
+    ProjectedGradientDescent: Applies gradient descent followed by projection.
+    ProximalGradient: Uses a proximal step for constrained or regularized objectives.
+"""
