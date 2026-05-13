a948c9195ba2772ec9cbfb945396859aa1dab68b
Joshua W. Windle <joshua.w.windle@gmail.com>
2026-05-13 11:37:52 +0200

    Joshua Windle - add normalization package

---
+++ aiga/layers/normalization/__init__.py
@@ -0,0 +1,9 @@
+"""Normalization layers.
+
+This package is reserved for layers that normalize activations or features
+during training.
+
+Planned layers:
+    BatchNorm: Normalizes activations using batch statistics.
+    LayerNorm: Normalizes activations within each sample.
+"""
