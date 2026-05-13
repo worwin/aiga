d2baca579aff833a625f5e3357eb9743893d4001
Joshua W. Windle <joshua.w.windle@gmail.com>
2026-05-13 11:33:57 +0200

    Joshua Windle - add regularized optimizer package

---
+++ aiga/optimizers/regularized_family/__init__.py
@@ -0,0 +1,9 @@
+"""Regularized optimizer family.
+
+This package is reserved for optimizer variants that include explicit
+regularization behavior, such as weight decay or Tikhonov/L2 penalties.
+
+Planned topics:
+    Weight decay: Penalizes large parameter values during optimization.
+    Tikhonov regularization: Adds an L2-style penalty to the objective.
+"""
