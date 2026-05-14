1ad1a4f19afee047b9820c4b24477cbe4efb2f94
Joshua W. Windle <joshua.w.windle@gmail.com>
2026-05-14 09:20:06 +0200

    Export SGD family optimizers

--- aiga/optimizers/sgd_family/__init__.py
+++ aiga/optimizers/sgd_family/__init__.py
@@ -2,10 +2,12 @@
 
 This package contains optimizers based on stochastic gradient descent.
 
-Planned optimizers:
+Optimizers:
     SGD: Plain stochastic gradient descent.
     MomentumSGD: SGD with momentum.
     NesterovSGD: SGD with Nesterov accelerated gradient.
 """
 
 from .sgd import SGD
+from .momentum_sgd import MomentumSGD
+from .nesterov_sgd import NesterovSGD
